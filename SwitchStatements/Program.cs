7a4339b82b7c0f10636fa0fe34a9ad63f308fe10
Amoriss <amoriss@protonmail.com>
2021-08-22 14:32:36 -0500

    initial commit

---
+++ SwitchStatements/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SwitchStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("What is your favorite school subject?");
+            string userAnswer = Console.ReadLine();
+
+            switch (userAnswer)
+            {
+                case "Math":
+                    Console.WriteLine("Math is rad");
+                    break;
+                case "English":
+                    Console.WriteLine("English is cool");
+                    break;
+                case "Recess":
+                    Console.WriteLine("Recess is the best subject");
+                    break;
+                case "Lunch":
+                    Console.WriteLine("Lunch is second best");
+                    break;
+                case "History":
+                    Console.WriteLine("History is awesome");
+                    break;
+                default:
+                    Console.WriteLine("Pick a school subject");
+                    break; 
+            }
+
+        }
+    }
+}
